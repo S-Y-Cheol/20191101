6a95089c662118b1aca1dd6213dac5924bf7d406
unknown <mirjoaaa@gamil.com>
2022-06-07 22:50:07 +0900

    new

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App(){
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />}/>
-        <Route path='/detail' element={<Detail />} />
+        <Route path='/movie-detail' element={<Detail />} />
       </Routes>
     </HashRouter>
     );
